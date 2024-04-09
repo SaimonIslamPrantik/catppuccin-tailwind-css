fd3284f3a5f57addefda575bd07258ef8c07c0af
Saimon Islam Prantik <82695288+SaimonIslamPrantik@users.noreply.github.com>
2024-04-09 23:14:10 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        "cat-rose": "#f4dbd6",
+        "cat-rose-light": "#f0c6c6",
+        "cat-pink": "#f5bde6",
+        "cat-mauve": "#c6a0f6",
+        "cat-red": "#ed8796",
+        "cat-maroon": "#ee99a0",
+        "cat-peach": "#f5a97f",
+        "cat-yellow": "#eed49f",
+        "cat-green": "#a6da95",
+        "cat-teal": "#8bd5ca",
+        "cat-sky": "#91d7e3",
+        "cat-sapphire": "#7dc4e4",
+        "cat-blue": "#8aadf4",
+        "cat-lavender": "#b7bdf8",
+      }
+    },
+  },
+  plugins: [],
+}
+
